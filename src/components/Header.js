4f1c1c902f9d416b7176191e46ad8d746b1a708b
MTO1122 <semetro1122@gmail.com>
2022-01-10 09:20:23 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-typos */
+
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({title, onAdd, showAdd}) => {
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            <Button 
+              color={showAdd ? 'red':'green'} 
+              text={showAdd ? 'Close':'Add'} onClick={onAdd} />
+        </header>
+    )
+}
+
+Header.defaultProps= {
+    title: 'Task Tracker',
+}
+
+Header.proptypes = {
+    title: PropTypes.string.isRequired,
+}
+export default Header
